f5467fda7240c18576d98b5cbaec923654e73956
IharrSkurtu <tisotta@gmail.com>
2019-01-25 12:09:52 +0300

    commit2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-
+#comment to commit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
